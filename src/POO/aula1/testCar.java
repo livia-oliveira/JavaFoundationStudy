f9f4b5fbda7d97a69005ca984b0151af95da720a
livia-oliveira <livia0liveiraslv93@gmail.com>
2024-01-20 17:39:43 -0300

    Concepts class, objects, attributes.

---
+++ src/POO/aula1/testCar.java
@@ -0,0 +1,18 @@
+package POO.aula1;
+
+public class testCar {
+    public static void main(String[] args) {
+        Car van = new Car();
+        van.brand = "Fiat";
+        van.model = "Ducato";
+        van.passengersCount = 10;
+        van.fuelCapacity = 100;
+        van.fuelConsumption = 0.2;
+
+        System.out.println("Marca: "+van.brand);
+        System.out.println("Modelo: "+van.model);
+        System.out.println("Número de passageiros: "+van.passengersCount);
+        System.out.println("Capacidade de combustível: "+van.fuelCapacity);
+        System.out.println("Consumo de combustível: "+van.fuelConsumption);
+    }
+}
