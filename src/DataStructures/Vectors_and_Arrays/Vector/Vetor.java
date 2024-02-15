c188388353790480bf9fe57c6446bff6e3d62046
livia-oliveira <livia0liveiraslv93@gmail.com>
2024-02-14 21:59:23 -0300

    Introduction to vectors and arrays

---
+++ src/DataStructures/Vectors_and_Arrays/Vector/Vetor.java
@@ -0,0 +1,9 @@
+package DataStructures.Vectors_and_Arrays.Vector;
+
+public class Vetor {
+  private String[] elements;
+
+  public Vetor(int capacity){
+    this.elements = new String[capacity];
+  }
+}
---
+++ src/DataStructures/Vectors_and_Arrays/Vector_test/aula1.java
@@ -0,0 +1,9 @@
+package DataStructures.Vectors_and_Arrays.Vector_test;
+
+import DataStructures.Vectors_and_Arrays.Vector.Vetor;
+
+public class aula1 {
+  public static void main(String[] args) {
+    Vetor vetor = new Vetor(5);
+  }
+}
