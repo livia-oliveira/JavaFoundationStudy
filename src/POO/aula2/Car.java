88a1a85a7fdd051a2472c5e9adff567db41df4f6
livia-oliveira <livia0liveiraslv93@gmail.com>
2024-03-25 20:57:29 -0300

    Mais métodos criados

--- src/POO/aula2/Car.java
+++ src/POO/aula2/Car.java
@@ -10,4 +10,14 @@ public class Car {
   void showAutonomy(){
     System.out.println("A autonomia do carro é: "+ fuelCapacity * fuelConsumption+" km");
   }
+
+  double achieveAutonomy(){
+    System.out.println("Método obterAutonomia foi chamado");
+    return fuelCapacity * fuelConsumption;
+  }
+
+  double calculateFuel(double km){
+    double fuelQuantity = km/fuelConsumption;
+    return fuelQuantity;
+  }
 }
